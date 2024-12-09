2caf9fab32add5c2508924ea5433ed6e5235ec15
Brian Neal <bneal@primehealthservices.com>
2024-12-09 09:00:23 -0600

    set up day 9

--- Advent2024/DayNine.cs
+++ Advent2024/DayNine.cs
@@ -1,73 +1,41 @@
 ﻿namespace Advent2024;
-static class DayNine
-{
-    
+static class DayNine {
+	static readonly char[] input;
 
-    static readonly char[][] input;
-    static readonly int yBound;
-    static readonly int xBound;
+	static DayNine() {
+		var useTestData = true;
+		input = ReadInput(useTestData);
+	}
 
-    static DayNine()
-    {
-        var useTestData = false;
-        input = ReadInput(useTestData);
-        yBound = input.Length;
-        xBound = input[0].Length;
-    }
 
-    record Data(Dictionary<char, List<Position>> dict, Position pos);
-    record Antennae(Position pos, char freq);
+	public static Answer Run() {
+		var partOne = input.Unpack().Defragment().CalculateCheckSum();
 
-    public static Answer Run()
-    {
-        var antennasByFrequency = input.MapAntennaPositions().GroupBy(a => a.freq).ToDictionary(g => g.Key, g => g.ToList());
+		var partTwo = 0;
 
-        var partOne = antennasByFrequency
-            .SelectMany(kvp =>
-                kvp.Value
-                    .SelectMany((A, i) => kvp.Value
-                        .Skip(i + 1)
-                        .SelectMany(B =>
-                            new[] { new Position(2 * A.pos.x - B.pos.x, 2 * A.pos.y - B.pos.y), new Position(2 * B.pos.x - A.pos.x, 2 * B.pos.y - A.pos.y) })))
-            .Where(p => p.IsInBounds())
-            .Distinct()
-            .Count();
+		return new Answer(partOne.ToString(), partTwo.ToString());
 
-        var partTwo = 0;
+	}
 
-        return new Answer(partOne.ToString(), partTwo.ToString());
+	static char[] Unpack(this char[] input) {
+		return input;
+	}
 
-    }
-    static IEnumerable<Antennae> MapAntennaPositions(this char[][] input) =>
-        Enumerable.Range(0, yBound)
-            .SelectMany(y => Enumerable.Range(0, xBound)
-                .Select(x => new Antennae(new Position(x, y), input[y][x]))
-                .Where(a => a.freq != '.'));
+	static char[] Defragment(this char[] input) {
+		return input;
+	}
 
-    static bool IsInBounds(this Position position) =>
-        position.x >= 0 &&
-        position.y >= 0 &&
-        position.x < xBound &&
-        position.y < yBound;
+	static int CalculateCheckSum(this char[] input) {
+		return 0;
+	}
 
-    static char[][] ReadInput(bool useTestData) =>
-        useTestData
-            ? TestData.Split(Environment.NewLine).Select(line => line.ToCharArray()).ToArray()
-            : File.ReadAllLines(".\\Inputs\\DayEight.txt").Select(line => line.ToCharArray()).ToArray();
+	static char[] ReadInput(bool useTestData) =>
+		useTestData
+			? TestData.ToCharArray()
+			: File.ReadAllText(".\\Inputs\\DayNine.txt").ToCharArray();
 
-    static string TestData =>
-        """
-        ............
-        ........0...
-        .....0......
-        .......0....
-        ....0.......
-        ......A.....
-        ............
-        ............
-        ........A...
-        .........A..
-        ............
-        ............
+	static string TestData =>
+		"""
+        2333133121414131402
         """;
 }
