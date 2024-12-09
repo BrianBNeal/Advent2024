e02a9cb217f2329ef003b125001da67431755572
Brian Neal <bneal@primehealthservices.com>
2024-12-09 13:26:01 -0600

    wip

--- Advent2024/DayNine.cs
+++ Advent2024/DayNine.cs
@@ -1,6 +1,8 @@
-﻿namespace Advent2024;
+﻿using System.Text;
+
+namespace Advent2024;
 static class DayNine {
-	static readonly char[] input;
+	static readonly string input;
 
 	static DayNine() {
 		var useTestData = true;
@@ -17,22 +19,52 @@ static class DayNine {
 
 	}
 
-	static char[] Unpack(this char[] input) {
-		return input;
+	static string Unpack(this string input) {
+		var unpacked = new StringBuilder();
+		for (int i = 0; i < input.Length; i++) {
+
+			var c = (i % 2 == 0)
+				? (i / 2).ToString()[0]
+				: '.';
+			var timesToRepeat = int.Parse(input.ToString().Substring(i, 1));
+
+			unpacked.Append(new string(c, timesToRepeat));
+		}
+
+		return unpacked.ToString();
 	}
 
-	static char[] Defragment(this char[] input) {
-		return input;
+
+	static char[] Defragment(this string input) {
+		var map = input.ToArray();
+		var start = 0;
+		var end = map.Length - 1;
+
+		while (start < end) {
+			while (map[start] != '.') {
+				start++;
+			}
+			while (map[end] == '.') {
+				end--;
+			}
+			map[start] = map[end];
+			map[end] = '.';
+		}
+
+		return map;
 	}
 
-	static int CalculateCheckSum(this char[] input) {
-		return 0;
-	}
+	static int CalculateCheckSum(this char[] input) =>
+		input.Aggregate((0,0), (acc, c) => {
+			acc.Item1 += acc.Item2 * input[acc.Item2];
+			acc.Item2++;
+			return acc;
+		}).Item1;
 
-	static char[] ReadInput(bool useTestData) =>
+	static string ReadInput(bool useTestData) =>
 		useTestData
-			? TestData.ToCharArray()
-			: File.ReadAllText(".\\Inputs\\DayNine.txt").ToCharArray();
+			? TestData
+			: File.ReadAllText(".\\Inputs\\DayNine.txt");
 
 	static string TestData =>
 		"""
