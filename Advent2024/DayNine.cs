d6a9a77ac8684945436dba797e954f57c15a2b00
Brian Neal <bneal@primehealthservices.com>
2024-12-10 08:22:15 -0600

    wip

--- Advent2024/DayNine.cs
+++ Advent2024/DayNine.cs
@@ -5,61 +5,136 @@ static class DayNine {
 	static readonly string input;
 
 	static DayNine() {
-		var useTestData = true;
-		input = ReadInput(useTestData);
+		var testData = true;
+		input = ReadInput(testData);
 	}
 
 
 	public static Answer Run() {
-		var partOne = input.Unpack().Defragment().CalculateCheckSum();
+		var partOne = input
+			.ToIntegerArray()
+			.Unpack()
+			.Defragment()
+			.CalculateCheckSum();
 
-		var partTwo = 0;
+		var partTwo = input
+			.ToIntegerArray()
+			.Unpack()
+			.DefragmentWhole()
+			.CalculateCheckSumWithGaps();
 
 		return new Answer(partOne.ToString(), partTwo.ToString());
-
 	}
 
-	static string Unpack(this string input) {
-		var unpacked = new StringBuilder();
-		for (int i = 0; i < input.Length; i++) {
+	static Dictionary<long, long> fileSizes = new();
 
-			var c = (i % 2 == 0)
-				? (i / 2).ToString()[0]
-				: '.';
-			var timesToRepeat = int.Parse(input.ToString().Substring(i, 1));
-
-			unpacked.Append(new string(c, timesToRepeat));
-		}
-
-		return unpacked.ToString();
-	}
-
-
-	static char[] Defragment(this string input) {
+	static long[] DefragmentWhole(this long[] input) {
 		var map = input.ToArray();
-		var start = 0;
-		var end = map.Length - 1;
+		long start = 0;
+		long end = map.Length - 1;
 
 		while (start < end) {
-			while (map[start] != '.') {
+			var first = map[start];
+			var last = map[end];
+
+			while (map[start] > -1L && start < end) {
 				start++;
+				first = map[start];
 			}
-			while (map[end] == '.') {
+			while (map[end] < 0L) {
 				end--;
+				last = map[end];
 			}
-			map[start] = map[end];
-			map[end] = '.';
+			if (start <= end) {
+				var fileSize = fileSizes[map[end]];
+				var canFit = map.Skip((int)start).Take((int)fileSize).All(x => x < 0);
+				if (canFit) {
+					for (var i = 0; i < fileSize; i++) {
+						map[start + i] = map[end - i];
+						if (end > start) {
+							map[end - i] = -1;
+						}
+					}
+				} else {
+					for (var i = 0; i < fileSize; i++) {
+						end--;
+					}
+				}
+			}
 		}
 
 		return map;
 	}
 
-	static int CalculateCheckSum(this char[] input) =>
-		input.Aggregate((0,0), (acc, c) => {
-			acc.Item1 += acc.Item2 * input[acc.Item2];
-			acc.Item2++;
-			return acc;
-		}).Item1;
+
+
+	static long[] ToIntegerArray(this string input) =>
+		input.Select(c => c == '.' ? -1 : long.Parse(c.ToString())).ToArray();
+
+	static long[] Unpack(this long[] input) {
+		var unpacked = new List<long>();
+		for (int i = 0; i < input.Length; i++) {
+
+			var val = (i % 2 == 0)
+				? (i / 2)
+				: -1;
+
+			var fileId = (long)val;
+			var fileSize = (long)input[i];
+			for (int j = 0; j < fileSize; j++) {
+				unpacked.Add(fileId);
+			}
+			fileSizes[fileId] = fileSize;
+		}
+
+		return [.. unpacked];
+	}
+
+
+	static long[] Defragment(this long[] input) {
+		var map = input.ToArray();
+		var start = 0;
+		var end = map.Length - 1;
+
+		while (start < end) {
+			while (map[start] >= 0 && start < end) {
+				start++;
+			}
+			while (map[end] < 0) {
+				end--;
+			}
+			if (start <= end) {
+				map[start] = map[end];
+			}
+			if (end > start) {
+				map[end] = -1;
+			}
+		}
+
+		return map;
+	}
+
+	static long CalculateCheckSum(this long[] input) {
+		var nums = input.TakeWhile(c => c > -1).ToArray();
+		long sum = 0;
+		for (long i = 0; i < nums.Length; i++) {
+			var num = nums[i];
+			sum += num * i;
+		}
+		return sum;
+	}
+
+	static long CalculateCheckSumWithGaps(this long[] input) {
+		long sum = 0;
+		for (long i = 0; i < input.Length; i++) {
+			var num = input[i];
+			if (num > -1) {
+				sum += num * i;
+			}
+		}
+		return sum;
+	}
+
 
 	static string ReadInput(bool useTestData) =>
 		useTestData
